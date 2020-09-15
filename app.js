114426349119ff59af83467f90ba3694a9b059a7
subham <subham.iitrpr@gmail.com>
2020-09-16 00:25:53 +0530

    Builds root API endpoint

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const Express = require('express');
+const fetch = require('node-fetch');
 
 const app = Express();
 
-app.get('/', (req, res) => {
-    res.send(new Date().toLocaleTimeString());
+
+async function getAdvice(){
+    try{
+        let res = await fetch('https://api.adviceslip.com/advice');
+        let data = await res.json();
+        return data['slip']['advice'];
+    }catch(err){
+        console.error(err);
+        return "Something went wrong! :(";
+    }
+}
+
+
+app.get('/', async (req, res) => {
+    let advice=await getAdvice();
+    res.send(`${new Date().toLocaleTimeString()} - ${advice}` );
 })
 
-app.listen(process.env.PORT || 8000)
+
+app.get('/*', (req, res) => {
+    res.redirect('/')
+})
+
+app.listen(process.env.PORT || 3000, () => console.log(`Listening at port ${process.env.PORT || 3000}`))
